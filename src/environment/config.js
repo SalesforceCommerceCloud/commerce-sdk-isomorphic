2ef145eb5cc6ca7d62ef7fb2dc8cb5fcd86228a9
Brian Redmond <bredmond@salesforce.com>
2020-09-09 11:53:24 -0400

    Add example config

---
+++ src/environment/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  proxy: 'https://localhost:3000',
+  headers: {},
+  parameters: {
+    clientId: 'CLIENT_ID',
+    organizationId: 'ORGANIZATION_ID',
+    shortCode: 'SHORT_CODE',
+    siteId: 'SITE_ID',
+  },
+}
