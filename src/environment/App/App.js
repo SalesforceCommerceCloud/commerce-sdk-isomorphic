1ebba3a988d9365bd898975208fe5651b987e5a5
Brian Redmond <bredmond@salesforce.com>
2020-09-15 19:59:57 -0400

    DEMO START POINT

--- src/environment/App/App.js
+++ src/environment/App/App.js
@@ -1,105 +1,35 @@
 import React, {Component} from 'react';
-import {ShopperSearch} from 'lib';
-import {config} from '../config';
+import logo from './logo.svg';
 import './App.css';
-
-const searchClient = new ShopperSearch(config);
-console.log(searchClient);
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.searchClient = searchClient;
     this.state = {
-      result: '',
-      searchText: 'sony'
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+      imageSrc: logo,
+      text: "Edit src/App.js and save to reload."
+    };
   } 
-
-  handleChange(event) {
-    this.setState({searchText: event.target.value});
-  }
-
-  handleSubmit(event) {
-    this.doSearch(this.state.searchText)
-    event.preventDefault();
-  }
-
-  getToken = async () => {
-    const response = await fetch(
-      `/customer/shopper-customers/v1/organizations/${this.searchClient.clientConfig.parameters.organizationId}/customers/actions/login?siteId=${this.searchClient.clientConfig.parameters.siteId}&clientId=${this.searchClient.clientConfig.parameters.clientId}`,
-      {
-        body: "{ \"type\": \"guest\" }",
-        method: "POST",
-        headers: { "Content-Type": "application/json" }
-      }
-    );
-    this.state.token = response.headers.get('Authorization');
-  }
-
-  getSearchResults = async (text) => {
-    await this.getToken();
-    this.searchClient.clientConfig.headers.authorization = this.state.token;
-    console.log(searchClient.clientConfig.headers);
-    return await searchClient.productSearch({
-      parameters: { q: text }
-    });
-  }
-
-  async doSearch() {
-    this.setState({result: 'searching...'});
-    const results = await this.getSearchResults(this.state.searchText);
-    console.log(results?.hits[0]);
-    if (results?.hits[0]) {
-      this.setState({...results.hits[0]})
-    } else {
-      this.setState({productId: null})
-    }
-  }
-
-  async componentDidMount() {
-    this.doSearch(this.state.searchText)
-  }
 
   render() {
     return (
-      <div className="search-component">
-        <form className="search-bar" onSubmit={this.handleSubmit}>
-          <label className="search-label">
-            Product Search 
-            <input className="search-box" type="text" value={this.state.searchTerm} onChange={this.handleChange} />
-          </label>
-          <input className="search-button" type="submit" value="Search" />
-        </form>
-        <Product
-          currency={this.state.currency}
-          id={this.state.productId}
-          name={this.state.productName}
-          price={this.state.price}
-        />
-      </div>
-    )
-  }
-}
-
-function Product(props) {
-  if (!props.id) {
-    return (
-      <div className="product-component">
-        <h3>Nothing!</h3>
-      </div>
-    )
-  }
-
-  return (
-    <div className="product-component">
-      <h3>{props.name}</h3>
-      <p className="product-price">${props.price}<span className="product-currency"> {props.currency}</span></p>
-      <p className="product-id">ID {props.id}</p>
+      <div className="App">
+      <header className="App-header">
+        <img src={this.state.imageSrc} className="App-logo" alt="logo" />
+        <p>
+          {this.state.text}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
     </div>
-    );
+    )
+  }
 }
 
 export default App;
