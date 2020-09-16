1b42f9406bed19df952d4d30ba683e201b503905
bredmond-sf <53481005+bredmond-sf@users.noreply.github.com>
2020-09-16 10:00:12 -0400

    Br/implement initial sdk.w 7802410 (#4)
    Adds initial SDK with fixed apis and sample app

---
+++ generate.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import path from "path";
+
+import { copySync } from "fs-extra";
+import { generate } from "@commerce-apps/raml-toolkit";
+
+import { registerHelpers, registerPartials, setupApis } from "./scripts/utils";
+
+const API_DIRECTORY = process.env.COMMERCE_SDK_INPUT_DIR
+  ? path.resolve(process.env.COMMERCE_SDK_INPUT_DIR)
+  : path.join(__dirname, "apis");
+const OUTPUT_DIRECTORY = path.join(__dirname, "renderedTemplates");
+const STATIC_DIRECTORY = path.join(__dirname, "static");
+
+registerHelpers();
+registerPartials();
+
+console.log(`Creating SDK for ${API_DIRECTORY}`);
+
+copySync(STATIC_DIRECTORY, OUTPUT_DIRECTORY);
+
+setupApis(API_DIRECTORY, OUTPUT_DIRECTORY).then((apis: generate.ApiMetadata) =>
+  apis.render()
+);
