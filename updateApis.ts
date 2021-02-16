489c06789a8440a83253eed33043543821489218
Will Harney <wharney@salesforce.com>
2021-02-16 15:29:17 -0500

    Appease linter!

--- updateApis.ts
+++ updateApis.ts
@@ -5,27 +5,25 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import path from "path";
-import fs from "fs-extra";
-import { updateApis } from "./scripts/utils";
+import path from 'path';
+import fs from 'fs-extra';
+import { updateApis } from './scripts/utils';
 
 const API_NAMES = [
-  "shopper-baskets",
-  "shopper-customers",
-  "shopper-gift-certificates",
-  "shopper-login",
-  "shopper-orders",
-  "shopper-products",
-  "shopper-promotions",
-  "shopper-search"
+  'shopper-baskets',
+  'shopper-customers',
+  'shopper-gift-certificates',
+  'shopper-login',
+  'shopper-orders',
+  'shopper-products',
+  'shopper-promotions',
+  'shopper-search',
 ];
 
-const PRODUCTION_API_PATH = path.join(__dirname, "apis");
+const PRODUCTION_API_PATH = path.join(__dirname, 'apis');
 
 // DOWNLOAD PRODUCTION DATA
 fs.remove(PRODUCTION_API_PATH);
 fs.ensureDirSync(PRODUCTION_API_PATH);
 
-API_NAMES.map((name) =>
-  updateApis(name, /production/i, PRODUCTION_API_PATH)
-);
+API_NAMES.map((name) => updateApis(name, /production/i, PRODUCTION_API_PATH));
