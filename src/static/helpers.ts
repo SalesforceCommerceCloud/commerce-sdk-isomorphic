451b790ccf634cbab6658dd4fbd8ee3c6fd2616f
Will Harney <wharney@salesforce.com>
2021-11-15 15:02:24 -0500

    Use CompositeParameters as more specific type.

--- src/static/helpers.ts
+++ src/static/helpers.ts
@@ -5,11 +5,6 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { CommonBaseUriParameters } from 'lib/commonParameters';
-
-export type RequiredKeys<T> = { [K in keyof T]-?: {} extends Pick<T, K> ? never : K; }[keyof T];
-export type PartialSubset<T extends object, K extends keyof any> = Omit<T, K> & Partial<T>;
 export type CompositeParameters<MethodParameters extends object, ConfigParameters extends object> =
     & Omit<MethodParameters, keyof ConfigParameters>
     & Partial<MethodParameters>
-    & CommonBaseUriParameters
