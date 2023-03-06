414ff5850715d624df5f2193371a1638708760ed
Will Harney <62956339+wjhsf@users.noreply.github.com>
2023-03-06 12:27:41 -0500

    Add prettify helper to make types more readable. (#113)

--- src/static/helpers/types.ts
+++ src/static/helpers/types.ts
@@ -1,20 +1,36 @@
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2023, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+/**
+ * Makes a type easier to read.
+ */
+type Prettify<T> = NonNullable<{
+  [K in keyof T]: T[K];
+}>;
+
+/**
+ * Generates the types required on a method, based on those provided in the config.
+ */
 export type CompositeParameters<
   MethodParameters extends Record<string, unknown>,
   ConfigParameters extends Record<string, unknown>
-> = Omit<MethodParameters, keyof ConfigParameters> & Partial<MethodParameters>;
+> = Prettify<
+  Omit<MethodParameters, keyof ConfigParameters> & Partial<MethodParameters>
+>;
 
+/**
+ * If an object has a `parameters` property, and the `parameters` object has required properties,
+ * then the `parameters` property on the root object is marked as required.
+ */
 export type RequireParametersUnlessAllAreOptional<
   T extends {parameters?: Record<string, unknown>}
 > = Record<string, never> extends NonNullable<T['parameters']>
   ? T
-  : T & Required<Pick<T, 'parameters'>>;
+  : Prettify<T & Required<Pick<T, 'parameters'>>>;
 
 /**
  * Template parameters used in the base URI of all API endpoints. `version` will default to `"v1"`
