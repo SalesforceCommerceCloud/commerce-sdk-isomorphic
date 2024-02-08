0ea10806293973523661fe9fa0225f6f7829a0c3
Joel Uong <joel.uong@salesforce.com>
2024-02-08 11:18:55 -0500

    update unit test

--- src/test/parameters.test.ts
+++ src/test/parameters.test.ts
@@ -197,6 +197,6 @@ describe('Parameters', () => {
     const response = await customersClient.authorizeCustomer(options);
 
     expect(response).toEqual(MOCK_RESPONSE);
-    expect(warnSpy).toHaveBeenCalledWith('Invalid Parameter: invalidParameter');
+    expect(warnSpy).toHaveBeenCalledWith('Invalid Parameter for authorizeCustomer: invalidParameter');
   });
 });
