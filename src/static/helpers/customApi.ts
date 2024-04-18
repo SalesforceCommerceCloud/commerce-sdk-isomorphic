fef40ae296e0774b558f58dedddaa8d248f4c7fc
Joel Uong <joel.uong@salesforce.com>
2024-04-18 16:22:16 -0400

    Refactor and add tsdoc comments

--- src/static/helpers/customApi.ts
+++ src/static/helpers/customApi.ts
@@ -8,22 +8,27 @@ import {BaseUriParameters, PathParameters} from 'lib/helpers';
 import TemplateURL from '../templateUrl';
 import type {FetchOptions} from '../clientConfig';
 import ResponseError from '../responseError';
-import {isBrowser, fetch} from './environment';
+import {fetch} from './environment';
 import {ClientConfigInit} from '../clientConfig';
 
-// TODO: check if you can pull out version in javascript, that way this doesn't have to be an .hbs file
-// import { USER_AGENT_HEADER, USER_AGENT_VALUE } from "../version";
-const USER_AGENT_HEADER = 'user-agent';
-const USER_AGENT_VALUE = 'commerce-sdk-isomorphic@1.13.1';
-
-// TODO: add js/tsdoc comment
-export const runFetchHelper = async <Params extends BaseUriParameters>( // TODO: also potentially extend { baseUri: string }
-  options: {
-    method: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
-    parameters?: {
-      [key: string]: string | number | boolean | string[] | number[];
-    };
-    path: string;
+/**
+ * A wrapper function around fetch designed for making requests using the SDK
+ * @param url - The url of the resource that you wish to fetch
+ * @param options? - An object containing any custom settings you want to apply to the request
+ * @param options.method? - The request HTTP operation. The available options are: 'GET', 'POST', 'PATCH', 'PUT', and 'DELETE'. 'GET' is the default if no method is provided.
+ * @param options.headers? - Headers that are added to the request. Authorization header should be in this argument or in the clientConfig.headers
+ * @param options.body? - Body that is used for the request
+ * @param clientConfig? - Client Configuration object used by the SDK with properties that can affect the fetch call
+ * @param clientConfig.headers? - Additional headers that are added to the request. Authorization header should be in this argument or in the options?.headers. options?.headers will override any duplicate properties.
+ * @param clientConfig.fetchOptions? - fetchOptions that are passed onto the fetch request
+ * @param clientConfig.throwOnBadResponse? - flag that when set true will throw a response error if the fetch request fails
+ * @param rawResponse? - Flag to return the raw response from the fetch call. True for raw response object, false for the data from the response
+ * @returns Raw response or data from response based on rawResponse argument from fetch call
+ */
+export const runFetchHelper = async <Params extends BaseUriParameters>(
+  url: string,
+  options?: {
+    method?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
     headers?: {
       authorization?: string;
     } & {[key: string]: string};
@@ -33,44 +38,28 @@ export const runFetchHelper = async <Params extends BaseUriParameters>( // TODO:
       | URLSearchParams
       | (BodyInit & (BodyInit | null));
   },
-  clientConfig: ClientConfigInit<Params>,
+  clientConfig?: ClientConfigInit<Params>,
   rawResponse?: boolean
-): Promise<Response | string> => {
-  const url = new TemplateURL(options.path, clientConfig.baseUri as string, {
-    pathParams: clientConfig.parameters as unknown as PathParameters,
-    queryParams: options?.parameters,
-    origin: clientConfig.proxy,
-  });
-
+): Promise<Response | unknown> => {
   const headers: Record<string, string> = {
     ...clientConfig?.headers,
     ...options?.headers,
   };
 
-  // TODO: potentially pull this out of helper method
-  // and leave it in the template
-  if (!isBrowser) {
-    // Browsers forbid setting a custom user-agent header
-    headers[USER_AGENT_HEADER] = [
-      headers[USER_AGENT_HEADER],
-      USER_AGENT_VALUE,
-    ].join(' ');
-  }
-
   const requestOptions: FetchOptions = {
-    ...clientConfig.fetchOptions,
+    ...clientConfig?.fetchOptions,
     headers,
     // TODO: probably need to fix this type
-    body: options.body as unknown as FormData & URLSearchParams,
-    method: options.method,
+    body: options?.body as unknown as FormData & URLSearchParams,
+    method: options?.method ?? 'GET',
   };
 
-  const response = await fetch(url.toString(), requestOptions);
+  const response = await fetch(url, requestOptions);
   if (rawResponse) {
     return response;
   }
   if (
-    clientConfig.throwOnBadResponse &&
+    clientConfig?.throwOnBadResponse &&
     !response.ok &&
     response.status !== 304
   ) {
@@ -78,7 +67,7 @@ export const runFetchHelper = async <Params extends BaseUriParameters>( // TODO:
   } else {
     const text = await response.text();
     // It's ideal to get "{}" for an empty response body, but we won't throw if it's truly empty
-    return (text ? JSON.parse(text) : {}) as string | Response;
+    return (text ? JSON.parse(text) : {}) as unknown | Response;
   }
 };
 
@@ -91,10 +80,25 @@ export interface CustomParams {
   [key: string]: unknown;
 }
 
-// TODO: add js/tsdoc comment
+/**
+ * A helper function designed to make calls to a custom API endpoint
+ * For more information about custom APIs, please refer to the [API documentation](https://developer.salesforce.com/docs/commerce/commerce-api/guide/custom-apis.html?q=custom+API)
+ * @param options - An object containing any custom settings you want to apply to the request
+ * @param options.method? - The request HTTP operation. The available options are: 'GET', 'POST', 'PATCH', 'PUT', and 'DELETE'. 'GET' is the default if no method is provided.
+ * @param options.parameters? - Query parameters that are added to the request
+ * @param options.headers? - Headers that are added to the request. Authorization header should be in this parameter or in the clientConfig.headers
+ * @param options.body? - Body that is used for the request
+ * @param clientConfig - Client Configuration object used by the SDK with properties that can affect the fetch call
+ * @param clientConfig.parameters - Path parameters used for custom API endpoints. The required properties are: apiName, endpointPath, organizationId, and shortCode. An error will be thrown if these are not provided.
+ * @param clientConfig.headers? - Additional headers that are added to the request. Authorization header should be in this argument or in the options?.headers. options?.headers will override any duplicate properties.
+ * @param clientConfig.fetchOptions? - fetchOptions that are passed onto the fetch request
+ * @param clientConfig.throwOnBadResponse? - flag that when set true will throw a response error if the fetch request fails
+ * @param rawResponse? - Flag to return the raw response from the fetch call. True for raw response object, false for the data from the response
+ * @returns Raw response or data from response based on rawResponse argument from fetch call
+ */
 export const callCustomEndpoint = async (
   options: {
-    method: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
+    method?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
     parameters?: {
       [key: string]: string | number | boolean | string[] | number[];
     };
@@ -109,7 +113,7 @@ export const callCustomEndpoint = async (
   },
   clientConfig: ClientConfigInit<CustomParams>,
   rawResponse?: boolean
-): Promise<Response | string> => {
+): Promise<Response | unknown> => {
   const requiredArgs = [
     'apiName',
     'endpointPath',
@@ -134,12 +138,15 @@ export const callCustomEndpoint = async (
     clientConfigCopy.parameters.apiVersion = 'v1';
   }
 
-  return runFetchHelper(
+  const url = new TemplateURL(
+    '/organizations/{organizationId}/{endpointPath}',
+    clientConfigCopy.baseUri as string,
     {
-      ...options,
-      path: '/organizations/{organizationId}/{endpointPath}',
-    },
-    clientConfigCopy,
-    rawResponse
+      pathParams: clientConfigCopy.parameters as unknown as PathParameters,
+      queryParams: options.parameters,
+      origin: clientConfig.proxy,
+    }
   );
+
+  return runFetchHelper(url.toString(), options, clientConfigCopy, rawResponse);
 };
