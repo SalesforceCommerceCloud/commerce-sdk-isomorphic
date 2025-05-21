34ece5edf6359578aaba77a2759fac3dabad3598
vcua-mobify <vcua@salesforce.com>
2025-05-20 19:03:29 -0700

    Add version file to static

---
+++ src/static/version.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// TODO: make this dynamic
+export const USER_AGENT_HEADER = 'user-agent';
+export const USER_AGENT_VALUE = 'commerce-sdk-isomorphic@3.0.0';
