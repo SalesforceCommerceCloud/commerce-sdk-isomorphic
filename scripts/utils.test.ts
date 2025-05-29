00b774cccd89cd7b7f6bfc97310e9d4d9642e8ff
vcua-mobify <vcua@salesforce.com>
2025-05-29 13:49:30 -0700

    Delete raml-specific templates and scripts

--- scripts/utils.test.ts
+++ scripts/utils.test.ts
@@ -4,101 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import {download, generate} from '@commerce-apps/raml-toolkit';
+import {download} from '@commerce-apps/raml-toolkit';
 import {
-  registerHelpers,
-  registerPartials,
-  setupApis,
-  updateApis,
   downloadLatestApis,
 } from './utils';
-
-const Handlebars = generate.HandlebarsWithAmfHelpers;
-const API_DIRECTORY = `${__dirname}/../apis`;
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
-const pkg: {version: string} = require('../package.json');
-
-describe('registerHelper', () => {
-  it('registers our custom helpers', () => {
-    expect(Object.keys(Handlebars.helpers)).not.toEqual(
-      expect.arrayContaining(['addNamespace', 'formatForTsDoc'])
-    );
-
-    registerHelpers();
-
-    expect(Object.keys(Handlebars.helpers)).toEqual(
-      expect.arrayContaining(['addNamespace', 'formatForTsDoc'])
-    );
-  });
-});
-
-describe('registerPartials', () => {
-  it('registers our partials', () => {
-    expect(Object.keys(Handlebars.partials)).not.toEqual(
-      expect.arrayContaining(['dtoPartial', 'operationsPartial'])
-    );
-
-    registerPartials();
-
-    expect(Object.keys(Handlebars.partials)).toEqual(
-      expect.arrayContaining(['dtoPartial', 'operationsPartial'])
-    );
-  });
-});
-
-describe('setupApis', () => {
-  it('loads our API modes', async () => {
-    // Don't need to perform the init, and doing so will cause timeout.
-    jest.spyOn(generate.ApiMetadata.prototype, 'init').mockResolvedValue();
-    jest.spyOn(generate.ApiModel.prototype, 'init').mockResolvedValue();
-
-    const apis = await setupApis(
-      API_DIRECTORY,
-      `${__dirname}/../renderedTemplates`
-    );
-
-    expect(apis.name.original).toEqual('apis');
-    expect(apis.metadata.sdkVersion).toContain(pkg.version);
-    const children = apis.children.map(child => child.name.original);
-    expect(children).toEqual(
-      expect.arrayContaining([
-        'shopper-baskets-oas',
-        'shopper-customers-oas',
-        'shopper-products-oas',
-        'shopper-search-oas',
-      ])
-    );
-  });
-});
-
-describe('test updateApis script', () => {
-  it('throws error when no results', async () => {
-    await expect(
-      updateApis('noResults', /production/i, '/tmp')
-    ).rejects.toThrow("No results in Exchange for 'noResults'");
-  });
-
-  it('throws error when no exact match', async () => {
-    await expect(updateApis('noMatch', /production/i, '/tmp')).rejects.toThrow(
-      "No exact match in Exchange for 'noMatch'"
-    );
-  });
-
-  it('downloads when exact match', async () => {
-    await expect(
-      updateApis('shopper-customers', /production/i, '/tmp')
-    ).resolves.toBeUndefined();
-  });
-
-  it('throws error when download fails', async () => {
-    jest
-      .spyOn(download, 'downloadRestApis')
-      .mockRejectedValue(new Error('It failed.'));
-    await expect(
-      updateApis('shopper-customers', /production/i, '/tmp')
-    ).rejects.toThrow('Failed to download shopper-customers: It failed.');
-  });
-});
 
 describe('test downloadLatestApis script', () => {
   it('throws error when no results', async () => {
