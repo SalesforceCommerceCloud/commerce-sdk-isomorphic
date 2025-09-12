9138911237a48555c731bd39fa48c8ddac5f0e0d
Joel Uong <joel.uong@salesforce.com>
2025-09-12 14:26:55 -0400

    update copyright comment

--- scripts/generateFileList.ts
+++ scripts/generateFileList.ts
@@ -1,6 +1,5 @@
-#!/usr/bin/env ts-node
 /*
- * Copyright (c) 2023, Salesforce, Inc.
+ * Copyright (c) 2025, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
