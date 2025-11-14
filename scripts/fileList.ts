759b475879f2831eb26b2c8454cffc675e932724
Joel Uong <joel.uong@salesforce.com>
2025-11-13 22:42:54 -0500

    update APIs

--- scripts/fileList.ts
+++ scripts/fileList.ts
@@ -17,13 +17,16 @@ const apiNames = [
   'helpers',
   'shopperBaskets',
   'shopperBasketsv2',
+  'shopperConfigurations',
   'shopperConsents',
   'shopperContext',
+  'shopperCustomObjects',
   'shopperCustomers',
   'shopperExperience',
   'shopperGiftCertificates',
   'shopperLogin',
   'shopperOrders',
+  'shopperPayments',
   'shopperProducts',
   'shopperPromotions',
   'shopperSearch',
@@ -39,7 +42,7 @@ const commonDependencies = [
   {input: 'src/lib/version.ts', file: 'lib/version.js'},
 ];
 
-// Total APIs: 15
+// Total APIs: 18
 // Total common dependencies: 5
 
 export {apiNames, commonDependencies};
